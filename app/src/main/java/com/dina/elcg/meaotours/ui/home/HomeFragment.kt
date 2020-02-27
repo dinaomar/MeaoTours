d79bd695999d27996ba4a93848d0b1aa93bcd388
dios <dios@calldorado.com>
2020-02-27 21:51:14 +0200

    adding first screen

--- app/src/main/java/com/dina/elcg/meaotours/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dina/elcg/meaotours/ui/home/HomeFragment.kt
@@ -4,28 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.dina.elcg.meaotours.R
+import com.dina.elcg.meaotours.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val binding: FragmentHomeBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        homeViewModel.startDataQuery()
+        homeViewModel.exposedData.observe(viewLifecycleOwner, Observer {
+            
         })
-        return root
+        homeViewModel.queryDataErrorExposed.observe(viewLifecycleOwner, Observer {
+            showErrorDialog(it.message!!)
+        })
+        return binding.root
+    }
+
+    private fun showErrorDialog(message: String) {
+        activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton(
+                    R.string.ok
+                ) { dialog, id ->
+                    dialog.dismiss()
+                }
+                setTitle("Error")
+                setMessage(message)
+            }
+            builder.create()
+        }
     }
 }
--- app/src/main/java/com/dina/elcg/meaotours/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dina/elcg/meaotours/ui/home/HomeViewModel.kt
@@ -3,11 +3,21 @@ package com.dina.elcg.meaotours.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dina.elcg.meaotours.MeaoDataBase
+import com.google.firebase.firestore.QuerySnapshot
 
 class HomeViewModel : ViewModel() {
+    private var queryResult:MutableLiveData<QuerySnapshot> = MutableLiveData()
+    private val queryError:MutableLiveData<Exception> = MutableLiveData()
+    val exposedData:LiveData<QuerySnapshot> get() = queryResult
+    val queryDataErrorExposed:LiveData<Exception> get() = queryError
+    val isLoading : MutableLiveData<Boolean> = MutableLiveData(true)
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun startDataQuery(){
+        MeaoDataBase.meaoDataBase.collection("activities")
+            .get().addOnSuccessListener {
+                queryResult.postValue(it)
+                isLoading.postValue(false)
+            }.addOnFailureListener { queryError.postValue(it) }
     }
-    val text: LiveData<String> = _text
 }
