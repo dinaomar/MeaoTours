70330ddb81e0b99ea32abdb2c30bda8e27457767
dios <dios@calldorado.com>
2020-02-27 11:53:35 +0200

    adding Meao tours

---
+++ app/src/main/java/com/dina/elcg/meaotours/MeaoApplication.kt
@@ -0,0 +1,7 @@
+package com.dina.elcg.meaotours
+
+import android.app.Application
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MeaoApplication : Application() {
+}
---
+++ app/src/main/java/com/dina/elcg/meaotours/MeaoDataBase.kt
@@ -0,0 +1,7 @@
+package com.dina.elcg.meaotours
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+object MeaoDataBase {
+    val meaoDataBase = FirebaseFirestore.getInstance()
+}
